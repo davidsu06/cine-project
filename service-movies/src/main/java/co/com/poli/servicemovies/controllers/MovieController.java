686a1d0e09cfcd000c2d053d3be2e36fdaf9b576
David Soto <dsoto@vozy.co>
2021-09-11 14:43:09 -0500

    Movies microservice crud finished

--- service-movies/src/main/java/co/com/poli/servicemovies/controllers/MovieController.java
+++ service-movies/src/main/java/co/com/poli/servicemovies/controllers/MovieController.java
@@ -1,15 +1,13 @@
 package co.com.poli.servicemovies.controllers;
 
 import co.com.poli.servicemovies.entities.Movie;
-import co.com.poli.servicemovies.services.MovieServiceImpl;
-import co.com.poli.servicemovies.utils.Response;
-import co.com.poli.servicemovies.utils.ResponseBuilder;
+import co.com.poli.servicemovies.services.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,16 +15,49 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieController {
 
-    private final MovieServiceImpl movieService;
-    private final ResponseBuilder builder;
+    private final MovieService movieService;
 
-    @GetMapping
-    public Response getMovies() {
-        List<Movie> movies = movieService.findAll();
-        if (movies.isEmpty()) {
-            return builder.success(null, HttpStatus.NOT_FOUND);
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@Valid @RequestBody Movie movie, BindingResult result){
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().build();
         }
 
-        return builder.success(movies);
+        movieService.save(movie);
+        return ResponseEntity.ok(movie);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        List<Movie> movies = movieService.findAll();
+
+        if (movies.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable("id") Long id) {
+        Movie movie = movieService.findById(id);
+
+        if (movie == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Movie> deleteMovie(@PathVariable("id") Long id) {
+        Movie movie = movieService.findById(id);
+
+        if (movie == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        movieService.delete(movie);
+        return ResponseEntity.ok(movie);
     }
 }
